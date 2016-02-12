f7738bb36a7246261b03d127ec1394348fe36edb
james_folk <jamesfolk1@gmail.com>
2016-02-12 01:30:43 -0500

    added the exporting script. Updated the game engine
    Former-commit-id: 5755d1dc36f622f3ba085200b59a8a52c3163bd0

---
+++ APPLE/Export Assets/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Export Assets
+//
+//  Created by James Folk on 2/12/16.
+//  Copyright © 2016 NJLIGames Ltd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
