a97f009814b83916bbff178f5aa56abb10a1c66a
NJLIGames <jamesfolk1@gmail.com>
2016-09-01 23:17:07 -0400

    moved the sceneStates table into the sceneEntity creation
    Former-commit-id: e2189a26eeea07f86a67d134aad91abb7a62eb11
    Former-commit-id: dccc23cc28c211eb70a39f5f5f8ef37a396646b9

--- COMMON/assets/scripts/yappybirds/scenes/MenuScene/MenuSceneEntity.lua
+++ COMMON/assets/scripts/yappybirds/scenes/MenuScene/MenuSceneEntity.lua
@@ -6,137 +6,190 @@ MenuSceneEntity.__index = MenuSceneEntity
 local json = require('json')
 
 setmetatable(MenuSceneEntity, {
- __index = SceneEntity,
- __call = function (cls, ...)
- local self = setmetatable({}, cls)
- self:create(...)
- return self
- end,
-})
+    __index = SceneEntity,
+    __call = function (cls, ...)
+      local self = setmetatable({}, cls)
+      self:create(...)
+      return self
+    end,
+  })
 
 function MenuSceneEntity:className()
- return "MenuSceneEntity"
+  return "MenuSceneEntity"
 end
 
 function MenuSceneEntity:class()
- return self
+  return self
 end
 
 function MenuSceneEntity:superClass()
- return SceneEntity
+  return SceneEntity
 end
 
 function MenuSceneEntity:destroy()
--- print(" MenuSceneEntity:destroy()")
+  -- print(" MenuSceneEntity:destroy()")
 
- MenuSceneEntity.__gc(self)
- SceneEntity.destroy(self)
+  MenuSceneEntity.__gc(self)
+  SceneEntity.destroy(self)
 end
 
 function MenuSceneEntity:create(init)
- SceneEntity.create(self, init)
 
- 
--- print(" MenuSceneEntity:create(init)")
+  local sceneEntityInit =
+  {
+    name = init.name,
+    states =
+    {
+      {
+        name = "AboutSceneEntityState",
+        module = require "yappybirds.scenes.MenuScene.states.AboutSceneEntityState"
+      },
+      {
+        name = "AchievementsSceneEntityState",
+        module = require "yappybirds.scenes.MenuScene.states.AchievementsSceneEntityState"
+      },
+      {
+        name = "BoardselectSceneEntityState",
+        module = require "yappybirds.scenes.MenuScene.states.BoardselectSceneEntityState"
+      },
+      {
+        name = "CharactersSceneEntityState",
+        module = require "yappybirds.scenes.MenuScene.states.CharactersSceneEntityState"
+      },
+      {
+        name = "HighScoresSceneEntityState",
+        module = require "yappybirds.scenes.MenuScene.states.HighScoresSceneEntityState"
+      },
+      {
+        name = "LeaderboardsSceneEntityState",
+        module = require "yappybirds.scenes.MenuScene.states.LeaderboardsSceneEntityState"
+      },
+      {
+        name = "LevelselectSceneEntityState",
+        module = require "yappybirds.scenes.MenuScene.states.LevelselectSceneEntityState"
+      },
+      {
+        name = "LoadingMenuSceneEntityState",
+        module = require "yappybirds.scenes.MenuScene.states.LoadingMenuSceneEntityState"
+      },
+      {
+        name = "MainMenuSceneEntityState",
+        module = require "yappybirds.scenes.MenuScene.states.MainMenuSceneEntityState"
+      },
+      {
+        name = "ModeselectSceneEntityState",
+        module = require "yappybirds.scenes.MenuScene.states.ModeselectSceneEntityState"
+      },
+      {
+        name = "SettingsSceneEntityState",
+        module = require "yappybirds.scenes.MenuScene.states.SettingsSceneEntityState"
+      },
+    },
+
+    startStateName = "LoadingMenuSceneEntityState",
+    gameInstance = init.gameInstance
+  }
+
+  SceneEntity.create(self, sceneEntityInit)
+
+  -- print(" MenuSceneEntity:create(init)")
 end
 
 function MenuSceneEntity:__gc()
- 
+
 end
 
 function MenuSceneEntity:__tostring()
- 
- return json:stringify(self)
+
+  return json:stringify(self)
 end
 
-
-
 function MenuSceneEntity:isLoaded()
- return SceneEntity.isLoaded(self)
+  return SceneEntity.isLoaded(self)
 end
 
 function MenuSceneEntity:load()
- SceneEntity.load(self)
+  SceneEntity.load(self)
 
- print("MenuSceneEntity:load()")
+  print("MenuSceneEntity:load()")
 end
 
 function MenuSceneEntity:unLoad()
- SceneEntity.unLoad(self)
+  SceneEntity.unLoad(self)
 
--- print(" MenuSceneEntity:unLoad()")
+  -- print(" MenuSceneEntity:unLoad()")
 end
 
 function MenuSceneEntity:initialize()
- SceneEntity.initialize(self)
+  SceneEntity.initialize(self)
 
- print("MenuSceneEntity:initialize()")
+  print("MenuSceneEntity:initialize()")
 end
 
 function MenuSceneEntity:enter()
- SceneEntity.enter(self)
+  SceneEntity.enter(self)
 
--- print(" MenuSceneEntity:enter()")
+  -- print(" MenuSceneEntity:enter()")
 end
 
 function MenuSceneEntity:update(timeStep)
- SceneEntity.update(self, timeStep)
+  SceneEntity.update(self, timeStep)
 
--- print(" MenuSceneEntity:update("..timeStep..")")
+  -- print(" MenuSceneEntity:update("..timeStep..")")
 end
 
 function MenuSceneEntity:exit()
- SceneEntity.exit(self)
+  SceneEntity.exit(self)
 
--- print(" MenuSceneEntity:exit()")
+  -- print(" MenuSceneEntity:exit()")
 end
 
 function MenuSceneEntity:onMessage(message)
- SceneEntity.onMessage(self, message)
+  SceneEntity.onMessage(self, message)
 
--- print(" MenuSceneEntity:onMessage()")
+  -- print(" MenuSceneEntity:onMessage()")
 end
 
 function MenuSceneEntity:touchDown(touches)
- SceneEntity.touchDown(self, touches)
+  SceneEntity.touchDown(self, touches)
 
--- print(" MenuSceneEntity:touchDown()")
+  -- print(" MenuSceneEntity:touchDown()")
 end
 
 function MenuSceneEntity:touchUp(touches)
- SceneEntity.touchUp(self, touches)
+  SceneEntity.touchUp(self, touches)
 
--- print(" MenuSceneEntity:touchUp()")
+  -- print(" MenuSceneEntity:touchUp()")
 end
 
 function MenuSceneEntity:touchMove(touches)
- SceneEntity.touchMove(self, touches)
+  SceneEntity.touchMove(self, touches)
 
--- print(" MenuSceneEntity:touchMove()")
+  -- print(" MenuSceneEntity:touchMove()")
 end
 
 function MenuSceneEntity:touchCancelled(touches)
- SceneEntity.touchCancelled(self, touches)
+  SceneEntity.touchCancelled(self, touches)
 
--- print(" MenuSceneEntity:touchCancelled()")
+  -- print(" MenuSceneEntity:touchCancelled()")
 end
 
 function MenuSceneEntity:renderHUD()
- SceneEntity.renderHUD(self)
+  SceneEntity.renderHUD(self)
 
--- print(" MenuSceneEntity:renderHUD()")
+  -- print(" MenuSceneEntity:renderHUD()")
 end
 
 function MenuSceneEntity:pause()
- SceneEntity.pause(self)
+  SceneEntity.pause(self)
 
--- print(" MenuSceneEntity:pause()")
+  -- print(" MenuSceneEntity:pause()")
 end
 
 function MenuSceneEntity:unPause()
- SceneEntity.unPause(self)
+  SceneEntity.unPause(self)
 
--- print(" MenuSceneEntity:unPause()")
+  -- print(" MenuSceneEntity:unPause()")
 end
 
 return MenuSceneEntity
--- COMMON/assets/scripts/yappybirds/worlds/YappyBirds/states/MenuWorldEntityState.lua
+++ COMMON/assets/scripts/yappybirds/worlds/YappyBirds/states/MenuWorldEntityState.lua
@@ -27,7 +27,7 @@ function MenuWorldEntityState:superClass()
 end
 
 function MenuWorldEntityState:destroy()
---  print(" MenuWorldEntityState:destroy()")
+  -- print(" MenuWorldEntityState:destroy()")
 
   MenuWorldEntityState.__gc(self)
   WorldEntityState.destroy(self)
@@ -41,68 +41,18 @@ local init =
 
 function MenuWorldEntityState:create(init)
   WorldEntityState.create(self, init)
-
---  print(" MenuWorldEntityState:create(init)")
-  local myInit =
+  
+  local init =
   {
     name = "MenuScene",
-    states =
-    {
-      {
-        name = "AboutSceneEntityState",
-        module = require "yappybirds.scenes.MenuScene.states.AboutSceneEntityState"
-      },
-      {
-        name = "AchievementsSceneEntityState",
-        module = require "yappybirds.scenes.MenuScene.states.AchievementsSceneEntityState"
-      },
-      {
-        name = "BoardselectSceneEntityState",
-        module = require "yappybirds.scenes.MenuScene.states.BoardselectSceneEntityState"
-      },
-      {
-        name = "CharactersSceneEntityState",
-        module = require "yappybirds.scenes.MenuScene.states.CharactersSceneEntityState"
-      },
-      {
-        name = "HighScoresSceneEntityState",
-        module = require "yappybirds.scenes.MenuScene.states.HighScoresSceneEntityState"
-      },
-      {
-        name = "LeaderboardsSceneEntityState",
-        module = require "yappybirds.scenes.MenuScene.states.LeaderboardsSceneEntityState"
-      },
-      {
-        name = "LevelselectSceneEntityState",
-        module = require "yappybirds.scenes.MenuScene.states.LevelselectSceneEntityState"
-      },
-      {
-        name = "LoadingMenuSceneEntityState",
-        module = require "yappybirds.scenes.MenuScene.states.LoadingMenuSceneEntityState"
-      },
-      {
-        name = "MainMenuSceneEntityState",
-        module = require "yappybirds.scenes.MenuScene.states.MainMenuSceneEntityState"
-      },
-      {
-        name = "ModeselectSceneEntityState",
-        module = require "yappybirds.scenes.MenuScene.states.ModeselectSceneEntityState"
-      },
-      {
-        name = "SettingsSceneEntityState",
-        module = require "yappybirds.scenes.MenuScene.states.SettingsSceneEntityState"
-      },
-    },
-
-    startStateName = "LoadingMenuSceneEntityState",
     gameInstance = self:getEntityOwner():getGameInstance()
   }
 
   local MenuSceneEntity = require "yappybirds.scenes.MenuScene.MenuSceneEntity"
   assert(MenuSceneEntity, "SceneEntity class is nil")
-  self:getEntityOwner():getGameInstance():getEntityManager():addSceneEntity(MenuSceneEntity(myInit))
+  self:getEntityOwner():getGameInstance():getEntityManager():addSceneEntity(MenuSceneEntity(init))
 
-  local sceneEntity = self:getEntityOwner():getGameInstance():getEntityManager():getSceneEntity(myInit.name)
+  local sceneEntity = self:getEntityOwner():getGameInstance():getEntityManager():getSceneEntity(init.name)
   assert(sceneEntity, "SceneEntity is nil")
   sceneEntity:load()
   sceneEntity:initialize()
@@ -124,13 +74,13 @@ end
 function MenuWorldEntityState:load()
   WorldEntityState.load(self)
 
---  print(" MenuWorldEntityState:load()")
+  -- print(" MenuWorldEntityState:load()")
 end
 
 function MenuWorldEntityState:unLoad()
   WorldEntityState.unLoad(self)
 
---  print(" MenuWorldEntityState:unLoad()")
+  -- print(" MenuWorldEntityState:unLoad()")
 end
 
 function MenuWorldEntityState:enter()
@@ -138,7 +88,7 @@ function MenuWorldEntityState:enter()
 end
 
 function MenuWorldEntityState:update(timeStep)
---  print(" MenuWorldEntityState:update("..timeStep..")")
+  -- print(" MenuWorldEntityState:update("..timeStep..")")
 end
 
 function MenuWorldEntityState:exit()
@@ -146,51 +96,51 @@ function MenuWorldEntityState:exit()
 end
 
 function MenuWorldEntityState:onMessage(message)
---  print(" MenuWorldEntityState:onMessage()")
+  -- print(" MenuWorldEntityState:onMessage()")
 end
 
 function MenuWorldEntityState:touchDown(touches)
---  print(" MenuWorldEntityState:touchDown()")
+  -- print(" MenuWorldEntityState:touchDown()")
 end
 
 function MenuWorldEntityState:touchUp(touches)
---  print(" MenuWorldEntityState:touchUp()")
+  -- print(" MenuWorldEntityState:touchUp()")
 end
 
 function MenuWorldEntityState:touchMove(touches)
---  print(" MenuWorldEntityState:touchMove()")
+  -- print(" MenuWorldEntityState:touchMove()")
 end
 
 function MenuWorldEntityState:touchCancelled(touches)
---  print(" MenuWorldEntityState:touchCancelled()")
+  -- print(" MenuWorldEntityState:touchCancelled()")
 end
 
 function MenuWorldEntityState:renderHUD()
---  print(" MenuWorldEntityState:renderHUD()")
+  -- print(" MenuWorldEntityState:renderHUD()")
 end
 
 function MenuWorldEntityState:keyboardShow()
---  print(" MenuWorldEntityState:keyboardShow()")
+  -- print(" MenuWorldEntityState:keyboardShow()")
 end
 
 function MenuWorldEntityState:keyboardCancel()
---  print(" MenuWorldEntityState:keyboardCancel()")
+  -- print(" MenuWorldEntityState:keyboardCancel()")
 end
 
 function MenuWorldEntityState:keyboardReturn(text)
---  print(" MenuWorldEntityState:keyboardReturn()")
+  -- print(" MenuWorldEntityState:keyboardReturn()")
 end
 
 function MenuWorldEntityState:receivedMemoryWarning()
---  print(" MenuWorldEntityState:receivedMemoryWarning()")
+  -- print(" MenuWorldEntityState:receivedMemoryWarning()")
 end
 
 function MenuWorldEntityState:pause()
---  print(" MenuWorldEntityState:pause()")
+  -- print(" MenuWorldEntityState:pause()")
 end
 
 function MenuWorldEntityState:unPause()
---  print(" MenuWorldEntityState:unPause()")
+  -- print(" MenuWorldEntityState:unPause()")
 end
 
 return MenuWorldEntityState
